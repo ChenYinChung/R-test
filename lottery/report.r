e8c2569b6d99a96fafac1df8dcc2947cd0c0d85c
sammy <sammy.chung@nexiosoft.com>
2020-03-09 19:04:51 +0800

    refactor unit entry

---
+++ lottery/report.r
@@ -0,0 +1,11 @@
+source("/Users/sammy/git/survey/r/lottery/ssc.r")
+
+print("================= 18LUCK")
+main("18LUCK",'NYSSC30S','2020-03-01','2020-03-09')
+print("================= 18LUCK======================")
+
+
+print("================= MANBETX")
+main("MANBETX",'NYSSC30S','2020-03-01','2020-03-09')
+print("================= MANBETX======================")
+
